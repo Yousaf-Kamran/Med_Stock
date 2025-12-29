848b14b4415880ee52ef09bb13a0f9539de5ddef
Yousaf kamran <yousaf.kamran4567@gmail.com>
2025-12-29 11:46:07 +0000

    install this on my project npm i @vercel/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { MedicineProvider } from "@/contexts/MedicineContext";
 import { ThemeProvider } from "@/components/ThemeProvider";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata: Metadata = {
   title: "MedStock Tracker",
@@ -40,6 +41,7 @@ export default function RootLayout({
             <Toaster />
           </MedicineProvider>
         </ThemeProvider>
+        <SpeedInsights />
       </body>
     </html>
   );
